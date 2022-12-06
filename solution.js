db723acfbec586c70d481e933450e5f479fb17f2
Rohit Prajapati <65508466+rohu0510@users.noreply.github.com>
2022-12-06 21:56:09 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -2,7 +2,7 @@
 
 function palindrome(str){
   const sLen = str.length;
-  for(let i=0; i<sLen; i++){
+  for(let i=0; i<sLen/2; i++){
     if (str[i] !== str[sLen - 1 - i]) {
       return false;
     }
