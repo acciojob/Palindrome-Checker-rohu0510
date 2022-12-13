d845a3d19e8de75b6e9dcc99fd514bb11ecbf85b
Rohit Prajapati <65508466+rohu0510@users.noreply.github.com>
2022-12-13 13:29:28 +0530

    final

--- solution.js
+++ solution.js
@@ -1,6 +1,7 @@
 // complete the given function
 
 function palindrome(str){
+  str = str.toLowerCase();
   const sLen = str.length;
   for(let i=0; i<sLen/2; i++){
     if (str[i] !== str[sLen - 1 - i]) {
